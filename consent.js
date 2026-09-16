9b34ff6860bdc56383e89f9c826eee7edcd3c184
juanmc98-maker <juanmc98@gmail.com>
2026-09-16 17:58:35 +0200

    Update consent.js

--- consent.js
+++ consent.js
@@ -1,14 +1,14 @@
 /* Consentimiento de cookies (analítica y publicidad). Versión 2026-09-16. */
 (function () {
 'use strict';
-var ID = 'G-CF5DB927SM', KEY = 'rt_privacy_v2', OLD = 'rt_consent';
+var ID = 'G-CF5DB927SM', PIXEL_ID = '1099906935714974', KEY = 'rt_privacy_v2', OLD = 'rt_consent';
 var VERSION = '2026-09-16', MAX_AGE = 365 * 86400000;
-var analyticsAllowed = false, adsAllowed = false, loaded = false, box, opener;
+var analyticsAllowed = false, adsAllowed = false, loaded = false, pixelLoaded = false, box, opener;
 var LANG = location.pathname.indexOf('/ca/') === 0 ? 'ca' : (location.pathname.indexOf('/en/') === 0 ? 'en' : 'es');
 var T = {
-es: { aria: 'Preferencias de cookies', html: '<p><strong>Tú decides sobre las cookies.</strong> Con tu permiso, medimos las visitas con Google Analytics y, si lo aceptas, usamos cookies de publicidad para medir la eficacia de nuestros anuncios (Google Ads). Puedes usar la web y los formularios aunque lo rechaces todo. Guardamos tu elección durante 12 meses. <a href="/cookies.html">Política de cookies</a>.</p><div class="choices"><button type="button" data-choice="reject">Rechazar todo</button><button type="button" data-choice="analytics">Solo analítica</button><button type="button" data-choice="all">Aceptar todo</button></div>' },
-ca: { aria: 'Preferències de galetes', html: '<p><strong>Tu decideixes sobre les galetes.</strong> Amb el teu permís, mesurem les visites amb Google Analytics i, si ho acceptes, fem servir galetes de publicitat per mesurar l\'eficàcia dels nostres anuncis (Google Ads). Pots utilitzar la web i els formularis encara que ho rebutgis tot. Guardem la teva elecció durant 12 mesos. <a href="/cookies.html">Política de galetes</a>.</p><div class="choices"><button type="button" data-choice="reject">Rebutjar-ho tot</button><button type="button" data-choice="analytics">Només analítica</button><button type="button" data-choice="all">Acceptar-ho tot</button></div>' },
-en: { aria: 'Cookie preferences', html: '<p><strong>You decide about cookies.</strong> With your permission, we measure visits with Google Analytics and, if you accept, we use advertising cookies to measure the effectiveness of our ads (Google Ads). You can use the website and forms even if you reject everything. We store your choice for 12 months. <a href="/cookies.html">Cookie policy</a>.</p><div class="choices"><button type="button" data-choice="reject">Reject all</button><button type="button" data-choice="analytics">Analytics only</button><button type="button" data-choice="all">Accept all</button></div>' }
+es: { aria: 'Preferencias de cookies', html: '<p><strong>Tú decides sobre las cookies.</strong> Con tu permiso, medimos las visitas con Google Analytics y, si lo aceptas, usamos cookies de publicidad para medir la eficacia de nuestros anuncios (Google Ads y Meta/Facebook). Puedes usar la web y los formularios aunque lo rechaces todo. Guardamos tu elección durante 12 meses. <a href="/cookies.html">Política de cookies</a>.</p><div class="choices"><button type="button" data-choice="reject">Rechazar todo</button><button type="button" data-choice="analytics">Solo analítica</button><button type="button" data-choice="all">Aceptar todo</button></div>' },
+ca: { aria: 'Preferències de galetes', html: '<p><strong>Tu decideixes sobre les galetes.</strong> Amb el teu permís, mesurem les visites amb Google Analytics i, si ho acceptes, fem servir galetes de publicitat per mesurar l\'eficàcia dels nostres anuncis (Google Ads i Meta/Facebook). Pots utilitzar la web i els formularis encara que ho rebutgis tot. Guardem la teva elecció durant 12 mesos. <a href="/cookies.html">Política de galetes</a>.</p><div class="choices"><button type="button" data-choice="reject">Rebutjar-ho tot</button><button type="button" data-choice="analytics">Només analítica</button><button type="button" data-choice="all">Acceptar-ho tot</button></div>' },
+en: { aria: 'Cookie preferences', html: '<p><strong>You decide about cookies.</strong> With your permission, we measure visits with Google Analytics and, if you accept, we use advertising cookies to measure the effectiveness of our ads (Google Ads and Meta/Facebook). You can use the website and forms even if you reject everything. We store your choice for 12 months. <a href="/cookies.html">Cookie policy</a>.</p><div class="choices"><button type="button" data-choice="reject">Reject all</button><button type="button" data-choice="analytics">Analytics only</button><button type="button" data-choice="all">Accept all</button></div>' }
 };
 window.dataLayer = window.dataLayer || [];
 window.gtag = function () { window.dataLayer.push(arguments); };
@@ -35,6 +35,20 @@ document.cookie = name + '=; Max-Age=0; path=' + path + (domain ? '; domain=' + 
 }); });
 });
 }
+function loadPixel() {
+if (pixelLoaded) return;
+pixelLoaded = true;
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+window.fbq('init', PIXEL_ID);
+window.fbq('track', 'PageView');
+}
 function applyConsent() {
 window.gtag('consent', 'update', {
 analytics_storage: analyticsAllowed ? 'granted' : 'denied',
@@ -43,6 +57,7 @@ ad_user_data: adsAllowed ? 'granted' : 'denied',
 ad_personalization: adsAllowed ? 'granted' : 'denied'
 });
 window['ga-disable-' + ID] = !analyticsAllowed;
+if (adsAllowed) loadPixel();
 if (!analyticsAllowed || loaded) return;
 loaded = true; window.__pmga = true;
 var s = document.createElement('script'); s.async = true; s.src = 'https://www.googletagmanager.com/gtag/js?id=' + ID; document.head.appendChild(s);
@@ -55,6 +70,7 @@ function disableAll() {
 analyticsAllowed = false; adsAllowed = false; window.__pmAnalyticsAllowed = false; window.__pmAdsAllowed = false;
 window['ga-disable-' + ID] = true;
 window.gtag('consent', 'update', {analytics_storage:'denied', ad_storage:'denied', ad_user_data:'denied', ad_personalization:'denied'});
+if (pixelLoaded && window.fbq) window.fbq('consent', 'revoke');
 clearCookies();
 }
 function hide() { if (box) box.hidden = true; if (opener && opener.isConnected) opener.focus(); }
@@ -68,6 +84,7 @@ disableAll();
 } else {
 analyticsAllowed = true; window.__pmAnalyticsAllowed = true;
 adsAllowed = ads; window.__pmAdsAllowed = ads;
+if (pixelLoaded && window.fbq && ads) window.fbq('consent', 'grant');
 applyConsent();
 }
 hide();
